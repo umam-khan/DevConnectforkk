3e161dc096df50f6b4975c2e92249ba136f870ad
asadparkar <58441390+asadparkar@users.noreply.github.com>
2023-03-07 19:25:19 +0530

    feat: Initialized React Router DOM and whole Login page with security is complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import Login from './Pages/Login'
+import Login from './Pages/Login';
+import Home from './Pages/Home';
+import {Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
-  
-
+  // const token = localStorage.getItem('jwt');
+  // const [tokenExists,setTokenExists] = useState(false)
+  // useEffect(()=>{
+  //   if (token){
+  //     setTokenExists(true);
+  //   }
+  // },[])
   return (
     <>
-      <Login />
+    <Routes>
+      <Route path='/' element={<Login />} />
+
+      <Route path='/home' element={<Home />} />
+    </Routes>
     </>
   )
 }
