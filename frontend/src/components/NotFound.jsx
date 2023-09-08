953da70950a8fa2b757fe9e31e7beb8cb532459c
ArunKumar <Kumararun2002@yahoo.com>
2023-09-08 23:58:05 +0530

    final commit

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -7,23 +7,23 @@ export default function NotFound() {
         display="inline-block"
         as="h2"
         size="2xl"
-        bgGradient="linear(to-r, teal.400, teal.600)"
+        bgGradient="linear(to-r, purple.400, purple.600)"
         backgroundClip="text">
         404
       </Heading>
       <Text fontSize="18px" mt={3} mb={2}>
         Page Not Found
       </Text>
-      <Text color={'gray.500'} mb={6}>
-        The page you're looking for does not seem to exist
+      <Text color={'gray.500'} mb={6} fontSize="6xl">
+      ඞSUSඞ
       </Text>
       <Link to="/">
       <Button
-        colorScheme="teal"
-        bgGradient="linear(to-r, teal.400, teal.500, teal.600)"
+        colorScheme="purple"
+        bgGradient="linear(to-r, purple.400, purple.500, purple.600)"
         color="white"
         variant="solid">
-        Go to Home
+        Back to Home
       </Button>
       </Link>
     </Box>
